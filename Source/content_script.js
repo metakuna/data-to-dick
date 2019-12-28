71c96500d314f6d0cfaa75dd10df40bafc5e5173
Will Howard <william.howard@exeter.ox.ac.uk>
2019-12-28 15:45:34 +0000

    changed cloud to data, butt to dick

--- Source/content_script.js
+++ Source/content_script.js
@@ -39,10 +39,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bdata\b/g, "dick");
+	v = v.replace(/\bData\b/g, "Dick");
+	// v = v.replace(/\bthe Cloud\b/g, "my Butt");
+	// v = v.replace(/\bthe cloud\b/g, "my butt");
 	
 	textNode.nodeValue = v;
 }
